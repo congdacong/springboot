8211d9d53c6ef2edaa7d3db87d7392e2476a1d71
CGB <303449744@qq.com>
2020-02-21 17:34:04 +0800

    log4j2

--- src/main/java/com/example/controller/IndexController.java
+++ src/main/java/com/example/controller/IndexController.java
@@ -2,7 +2,10 @@ package com.example.controller;
 
 import com.example.entity.Account;
 import com.example.service.AccountService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,10 +13,16 @@ import java.util.List;
 
 @RestController
 public class IndexController {
+    private static final org.slf4j.Logger logger = org.slf4j.LoggerFactory.getLogger(IndexController.class);
     @Autowired
     AccountService accountService;
-    @RequestMapping("index")
+    @RequestMapping("/index")
     public List<Account> index(){
+        logger.trace("trace level");
+        logger.debug("debug level");
+        logger.info("info level");
+        logger.warn("warn level");
+        logger.error("error level");
         return accountService.findAll();
     }
 }
