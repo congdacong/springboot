5c0e9c334ca59277ffaa2a7b1c7ed28be262b216
CGB <303449744@qq.com>
2020-03-26 23:28:50 +0800

    activiti7 流程实例 6/1 单元测试

--- src/main/java/com/example/activiti/component/processEngine.java
+++ src/main/java/com/example/activiti/component/processEngine.java
@@ -1,7 +1,9 @@
 package com.example.activiti.component;
 
 import org.activiti.engine.RepositoryService;
+import org.activiti.engine.RuntimeService;
 import org.activiti.engine.repository.Deployment;
+import org.activiti.engine.runtime.ProcessInstance;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,7 +14,9 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class processEngine {
         @Autowired
-        RepositoryService repositoryService;
+        private RepositoryService repositoryService;
+        @Autowired
+        private RuntimeService runtimeService;
         @Test
         /**流程定义：bpmn文件，一个定义对应多个流程实例。
          * 部署流程(非启动!)：部署信息表：ACT_RE_DEPLOYMENT  流程定义数据表：ACT_RE_PROCDEF 资源文件表：ACT_GE_BYTEARRAY
@@ -25,5 +29,17 @@ public class processEngine {
                         .addClasspathResource ("processes/process.png")
                         .name("请假申请")
                         .deploy ();
+        }
+
+        /**
+         * 流程实例
+         */
+        @Test
+        public void RuntimeProcess(){
+                ProcessInstance process = runtimeService.startProcessInstanceByKey("process");
+                System.out.println(process.getDeploymentId());
+                System.out.println(process.getName());
+                System.out.println(process.getId());
+
         }
 }
